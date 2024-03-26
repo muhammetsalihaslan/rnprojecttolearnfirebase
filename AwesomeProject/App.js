b23068b92af6657949fafa894d33cc395558da98
Your Name <you@example.com>
2024-03-26 12:57:19 +0300

    starting package

---
+++ AwesomeProject/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <Text style={{fontSize: 70}}>Hello Firebase</Text>
+    </SafeAreaView>
+  );
+};
